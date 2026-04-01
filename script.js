8848aab4c8a63a60c1fde58905dce234e9ddb28d
Arju Srivastav <arju.srivastav7@gmail.com>
2026-04-01 22:00:53 +0530

    Added the JS logic for showing the recipe cards

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const API_KEY = "https://www.themealdb.com/api/json/v1/1";
 
 const ingredientInput = document.getElementById("ingredient-input");
-const query = ingredientInput.value.trim();
 const searchBtn = document.getElementById("search-btn");
 const errMessage = document.getElementById("error-message");
+const recipeGrid = document.getElementById("recipe-grid");
+const resultSection = document.getElementById("results-section");
+const resultTitle = document.getElementById("results-title");
 
 
 async function handleSearch(query) {
@@ -19,7 +21,7 @@ async function handleSearch(query) {
         const data = await response.json();
         displayRecipes(data.meals, query);
     }catch (err){
-        console.log("Error occured: ", err);
+        // console.log("Error occured: ", err);
         errMessage.textContent = "Error fetching data. Please try again later.";
         errMessage.classList.remove("hidden");
     } finally {
@@ -29,5 +31,34 @@ async function handleSearch(query) {
 }
 
 function displayRecipes(recipes, query){
-    
+    recipeGrid.innerHTML = "";
+    resultSection.classList.remove("hidden")
+    if (!recipes){
+        resultTitle.textContent = `No recipes found for "${query}"`
+        return;
+    }else{
+        recipes.forEach(meal => {
+            const card = document.createElement("div");
+            card.className = "recipe-card";
+            card.innerHTML = `
+                <img src="${meal.strMealThumb}/preview" alt="${meal.strMeal}" class="recipe-card-img" loading="lazy">
+                <div class="recipe-card-content">
+                    <div class="recipe-card-meta">View Recipe</div>
+                    <h3 class="recipe-card-title" title="${meal.strMeal}">${meal.strMeal}</h3>
+                </div>
+            `;
+            recipeGrid.appendChild(card);
+        });
+    }
 }
+
+searchBtn.addEventListener("click", () => {
+    const query = ingredientInput.value.trim();
+    handleSearch(query);
+});
+ingredientInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        const query = ingredientInput.value.trim();
+        handleSearch(query);
+    }
+});
